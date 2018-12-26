eef6307516e4f561f70811bb35ce68c32fde7aaa
BernZuo <69089745@qq.com>
2018-12-26 15:53:54 +0800

    Initial commit

---
+++ Study.py
@@ -0,0 +1,8 @@
+# _*_ coding:utf-8 _*_
+# @Time     :17:46
+# @Author   :zwl
+# @Email    :
+# @File     :Study.py
+# @Software :PyCharm
+
+print(3/2)
---
+++ py2.py
@@ -0,0 +1,8 @@
+# _*_ coding:utf-8 _*_
+# @Time     :16:51
+# @Author   :zwl
+# @Email    :
+# @File     :py2.py
+# @Software :PyCharm
+
+print(3/2)
