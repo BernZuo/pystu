12d5d6e33b465dfec1e0732e841f8148f558e496
BernZuo <69089745@qq.com>
2019-01-04 17:59:26 +0800

    ss

--- 2.py
+++ 2.py
@@ -1,5 +1,6 @@
 from openpyxl import load_workbook
 import pymysql
+import arrow
 def doExcel(file_name,sheet_name):
     wd =load_workbook(file_name)
     sheet = wd[sheet_name]
@@ -27,24 +28,31 @@ def mySQL_01():
     conn=pymysql.connect(**config)
 
     # 使用cursor()方法获取操作游标
-
     cur = conn.cursor()
 
     try:
         # 创建数据库
+        createSql = ''
         DB_NAME = 'bill'
+        TB_NAME = 'dd' + arrow.now().shift(months=-1).format("YYYYMM") + 'bill'
         cur.execute('CREATE DATABASE IF NOT EXISTS %s' %DB_NAME)
         conn.select_db(DB_NAME)
-
+        #调用自定义doExcel方法，获取文件表头
         a = doExcel('大德12月服务接口调用量.xlsx', 'Sheet1')
+        for key in a[0].keys():
+            if isinstance(a[0][key],str):
+                createSql = createSql + key + ' varchar(255)' + ','
+            else:
+                createSql = createSql + key + ' int(11)' + ','
+        createSql = 'CREATE TABLE IF NOT EXISTS %s(%s)DEFAULT CHARSET=utf8' %(TB_NAME,createSql[:-1])
+        cur.execute(createSql)
         for i in a:
             data = i
-            print(i)
-            sql = "INSERT INTO `table_name`(客户标识,客户名称,数据源,运营商,服务项编码,接口,接口名称,承载平台,产品名称,总和调用量) VALUES('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(
-                data['客户标识'], data['客户名称'], data['数据源'], data['运营商'], data['服务项编码'], data['接口'], data['接口名称'], data['承载平台'],
-                data['产品名称'], data['总和调用量'])
-            #print(sql)
-            #cur.execute(sql)
+            #print(i)
+            sql = "INSERT INTO {}(客户标识,客户名称,数据源,运营商,服务项编码,接口,接口名称,承载平台,产品名称,总和调用量) VALUES('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')" .format(
+                TB_NAME,data['客户标识'], data['客户名称'], data['数据源'], data['运营商'], data['服务项编码'], data['接口'], data['接口名称'], data['承载平台'],data['产品名称'], data['总和调用量'])
+            print(sql)
+            cur.execute(sql)
         #获取结果  1）获取单条  2）获取多条
         # res=cur.fetchone()
         # res=cur.fetchall()
