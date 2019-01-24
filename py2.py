c15a3e034005b5c7b68c192b7f67a3d7adb43d2f
BernZuo <69089745@qq.com>
2019-01-24 10:26:04 +0800

    ss

--- py2.py
+++ py2.py
@@ -4,6 +4,19 @@
 # @Email    :
 # @File     :py2.py
 # @Software :PyCharm
+from __future__ import division
+'''
+列表
+字典
+元组
+'''
+# 列表 任意对象的有序集合 可变长度
+L1 = [1,2,3] + ['a','b','c']
+for x in L1:
+    print x
+L2 = ['abc',['def','ghi']]
+L2.append('b')
+L2.extend([5,6,7])
+print(L2)
 
-print(3/2)
-print(4/5)
+# 字典 无序 键值对
